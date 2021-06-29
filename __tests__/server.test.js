04291d35924abedeabe77c737670b7f6b107b49b
Ibrahim Abu-awad <ibrahimabuawadwork@gmail.com>
2021-06-29 15:22:46 +0300

    done

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,10 +3,22 @@ const supertest = require('supertest');
 const mockRequest = supertest(server);
 
 describe('api server',()=>{
-  it('Should return type',async ()=>{
+  it('Should return name',async ()=>{
     const query = 'ibrahim';
     const response = await mockRequest.get(`/person?name=${query}`);
     expect(response.body.name).toEqual(query);
     expect(response.status).toBe(200);
+  });
+  it('Should return name',async ()=>{
+    const query = 2;
+    const response = await mockRequest.get(`/person?name=${query}`);
+    
+    expect(response.status).toBe(500);
+  });
+  it('Should return name',async ()=>{
+
+    const response = await mockRequest.get(`/bad`);
+    
+    expect(response.status).toBe(404);
   });
 });
